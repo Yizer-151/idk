2cd3b1cc31a105e03c58f0d08c7d1d748866ff4b
Mithun <76549104+Agent-150@users.noreply.github.com>
2023-02-20 19:14:23 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -16,9 +16,7 @@ async def start(client, message):
         await db.add_user(user.id)             
     txt=f"👋 Hello Developer {user.mention} \n\nI am an Advance file Renamer and file Converter BOT with Custom thumbnail support.\n\nSend me any video or document !"
     button=InlineKeyboardMarkup([[
-        InlineKeyboardButton(' JOIN', url='https://telegram.me/Anime_Vertix'),
-        ]
-        ])
+        InlineKeyboardButton(' JOIN', url='https://telegram.me/Anime_Vertix')]])
     if START_PIC:
         await message.reply_photo(START_PIC, caption=txt, reply_markup=button)       
     else:
