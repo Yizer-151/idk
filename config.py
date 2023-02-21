97b1d00a5afa6c4c4704ebbf69578756ff0f0dd2
Mithun <76549104+Agent-150@users.noreply.github.com>
2023-02-21 17:40:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ DB_URL = os.environ.get("DB_URL","")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "https://graph.org/Rename-Bot-01-15")
+START_PIC = os.environ.get("START_PIC", "https://graph.org/Anime-Vertix-02-21")
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
